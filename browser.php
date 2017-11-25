a465e829e8152b1b7d51505c0b523bbd6f5dd988
Tomáš Kolinger <tomas@kolinger.name>
2017-11-25 09:45:08 +0100

    added info for robots

--- browser.php
+++ browser.php
@@ -129,6 +129,7 @@ if ($authenticated) {
 		<meta charset=utf-8 />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 		<meta name="viewport" content="width=device-width,initial-scale=1" />
+		<meta name="robots" content="noindex, nofollow" />
 
 		<title><?php echo $title ?></title>
 
